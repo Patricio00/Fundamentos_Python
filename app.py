b29d471c5297740351d2e35a2ef801f362688fdf
Patricio00 <102130701+Patricio00@users.noreply.github.com>
2026-09-25 10:30:32 -0500

    Add module selection and display messages

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ st.title("Python Fundamentals")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Patricio Jarrín")
 modulo = st.sidebar.selectbox("Seleccione un módulo",["Listas","Arreglos","Funciones","POO"])
+if modulo == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+elif modulo == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+elif modulo == "Funciones":
+  st.write("Te encuentras en el módulo de funciones")
+else:
+  st.write("Te encuentras en el módulo de POO")
+  
