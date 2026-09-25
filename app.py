77d1618ec977b72ad69f68acbfdaef081ff1af5c
Patricio00 <102130701+Patricio00@users.noreply.github.com>
2026-09-25 10:24:45 -0500

    Add author information and module selection to app

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("Python Fundamentals")
 st.sidebar.title("Parámetros")
+st.write("Elaborado por: Patricio Jarrín")
+modulo = st.sidebar.selectbox("Seleccione un módulo",["Listas","Arreglos","Funciones","POO"])
