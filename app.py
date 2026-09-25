6b1ed876a826ea85a315d24b2390863656628ace
Patricio00 <102130701+Patricio00@users.noreply.github.com>
2026-09-25 10:36:34 -0500

    Add number input for initial and final values

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ st.write("Elaborado por: Patricio Jarrín")
 modulo = st.sidebar.selectbox("Seleccione un módulo",["Listas","Arreglos","Funciones","POO"])
 if modulo == "Listas":
   st.write("Te encuentras en el módulo de listas")
+  valor_inicial = st.number_input("Ingresa tu valor inicial",min_value = 0,max_value = 100,value = 10)
+  valor_final = st.number_input("Ingresa tu valor final",min_value = 0,max_value = 100,value = 20)
+  lista = list(range(int(valor_inicial),int(valor_final)))
+  st.write(lista)
 elif modulo == "Arreglos":
   st.write("Te encuentras en el módulo de arreglos")
 elif modulo == "Funciones":
