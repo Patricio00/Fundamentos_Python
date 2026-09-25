6d0e6c0dece5d9b9f9b8f2446ceb592d72dcd1a8
Patricio00 <102130701+Patricio00@users.noreply.github.com>
2026-09-25 10:40:47 -0500

    Replace list range with numpy arange for array creation

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ elif modulo == "Arreglos":
   st.write("Te encuentras en el módulo de arreglos")
   valor_inicial_array = st.number_input("Ingresa tu valor inicial del arreglo",min_value = 0,max_value = 100,value = 10)
   valor_final_array = st.number_input("Ingresa tu valor final del arreglo",min_value = 0,max_value = 100,value = 20)
-  arreglo = list(range(int(valor_inicial_array),int(valor_final_array)))
+  arreglo = np.arange(int(valor_inicial_array),int(valor_final_array))
   st.write(arreglo)  
 elif modulo == "Funciones":
   st.write("Te encuentras en el módulo de funciones")
